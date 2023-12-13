7b6c37e1fd4df15b46b2989b4afe759cfafd6a47
Ahinsa Sathsarani <121052547+ahinsa2001@users.noreply.github.com>
2023-12-13 16:04:19 +0530

    Securentra Project Files

---
+++ AccessCheckingForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Securentra_Management_System
+{
+    partial class AccessCheckingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accessTag = new System.Windows.Forms.Label();
+            this.ownerId = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.plateno = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // accessTag
+            // 
+            this.accessTag.AutoSize = true;
+            this.accessTag.BackColor = System.Drawing.Color.Transparent;
+            this.accessTag.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accessTag.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(64)))), ((int)(((byte)(123)))));
+            this.accessTag.Location = new System.Drawing.Point(219, 108);
+            this.accessTag.Name = "accessTag";
+            this.accessTag.Size = new System.Drawing.Size(382, 44);
+            this.accessTag.TabIndex = 12;
+            this.accessTag.Text = "ACCESS PERMISSION";
+            this.accessTag.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ownerId
+            // 
+            this.ownerId.AutoSize = true;
+            this.ownerId.BackColor = System.Drawing.Color.Transparent;
+            this.ownerId.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ownerId.ForeColor = System.Drawing.Color.Navy;
+            this.ownerId.Location = new System.Drawing.Point(493, 238);
+            this.ownerId.Name = "ownerId";
+            this.ownerId.Size = new System.Drawing.Size(39, 20);
+            this.ownerId.TabIndex = 28;
+            this.ownerId.Text = "------";
+            this.ownerId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(288, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Owner ID : -";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Navy;
+            this.label5.Location = new System.Drawing.Point(288, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 20);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Vehicle PlateNumber : -";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plateno
+            // 
+            this.plateno.AutoSize = true;
+            this.plateno.BackColor = System.Drawing.Color.Transparent;
+            this.plateno.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plateno.ForeColor = System.Drawing.Color.Navy;
+            this.plateno.Location = new System.Drawing.Point(493, 205);
+            this.plateno.Name = "plateno";
+            this.plateno.Size = new System.Drawing.Size(0, 20);
+            this.plateno.TabIndex = 25;
+            this.plateno.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Exit
+            // 
+            this.Exit.AutoSize = true;
+            this.Exit.BackColor = System.Drawing.Color.Transparent;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(93)))), ((int)(((byte)(142)))));
+            this.Exit.Location = new System.Drawing.Point(790, 9);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(19, 18);
+            this.Exit.TabIndex = 29;
+            this.Exit.Text = "X";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(288, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Date : -";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(288, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Time : -";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.BackColor = System.Drawing.Color.Transparent;
+            this.Time.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Time.ForeColor = System.Drawing.Color.Navy;
+            this.Time.Location = new System.Drawing.Point(493, 304);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(39, 20);
+            this.Time.TabIndex = 33;
+            this.Time.Text = "------";
+            this.Time.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Date
+            // 
+            this.Date.AutoSize = true;
+            this.Date.BackColor = System.Drawing.Color.Transparent;
+            this.Date.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Date.ForeColor = System.Drawing.Color.Navy;
+            this.Date.Location = new System.Drawing.Point(493, 271);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(39, 20);
+            this.Date.TabIndex = 32;
+            this.Date.Text = "------";
+            this.Date.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // num
+            // 
+            this.num.AutoSize = true;
+            this.num.BackColor = System.Drawing.Color.Transparent;
+            this.num.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num.ForeColor = System.Drawing.Color.Navy;
+            this.num.Location = new System.Drawing.Point(493, 205);
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(39, 20);
+            this.num.TabIndex = 34;
+            this.num.Text = "------";
+            this.num.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // AccessCheckingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Securentra_Management_System.Properties.Resources._6e32878542a4a6b86e640204d951fbff__blue_wallpapers_blue_backgrounds;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(821, 427);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.ownerId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.plateno);
+            this.Controls.Add(this.accessTag);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AccessCheckingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AccessCheckingForm";
+            this.Load += new System.EventHandler(this.AccessCheckingForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label accessTag;
+        private System.Windows.Forms.Label ownerId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label plateno;
+        private System.Windows.Forms.Label Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.Label Date;
+        private System.Windows.Forms.Label num;
+    }
+}
---
+++ AccessCheckingForm.cs
@@ -0,0 +1,59 @@
+﻿using Emgu.CV.Bioinspired;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Securentra_Management_System
+{
+    public partial class AccessCheckingForm : Form
+    {
+        public AccessCheckingForm(string platenumber, string date, string time, string owner, string accessStatus)
+        {
+            InitializeComponent();
+            Date.Text = date;
+            Time.Text = time;
+            num.Text = platenumber;
+            ownerId.Text = owner;
+            accessTag.Text = accessStatus;
+        }
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "ClOVtlKcXyNZP4W4Bvai2DgxtQLPcNWY417W0nk4",
+            BasePath = "https://ntadb-95601-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient client;
+
+        private void AccessCheckingForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+            }
+            catch
+            {
+                MessageBox.Show("No Internet or Connection Problem");
+            }
+
+            if(accessTag.Text == "ACCESS SUCCESSFULLY")
+            {
+                accessTag.ForeColor = Color.Green;
+            }
+            else if(accessTag.Text == "ACCESS DENIED")
+            {
+                accessTag.ForeColor = Color.Red;
+            }
+            
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
